ff390c0561cb23849fed040b24451b5a392c4492
braydenwhitlock <braydenwhitlock@gmail.com>
2021-10-13 13:04:56 -0600

    added a while loop with some random numbers

--- numberTwo.java
+++ numberTwo.java
@@ -4,7 +4,12 @@ class numberTwo{
 		for(int i = 0; i < n; i++){
 			if(i%3 == 0 && i != 0){ 
 				System.out.println("The number is: " + i);
-			}		
+			}
+		}
+		double w = 1;
+		while(w < 98){
+			System.out.println("This is the next test number: " + w);		
+			w = java.lang.Math.random()*100;
 		}
 	}
 }
